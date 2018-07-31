8f0bfa20e7b347bd9ef364ccdd16beef0792a180
Ray <ryan.natoor@gmail.com>
2018-07-31 15:56:05 +0300

    #5 added currency list json file

--- src/router.js
+++ src/router.js
@@ -1,23 +1,17 @@
 const handlers = require("./handlers");
 
-const router = functions(req, res) {
-  let url = req.url;
+const router = (req, res) => {
+  const url = req.url;
 
-  if(url ==="/") {
-    handlers.handlerHome(req, res)
-  }
-
-  else if(url.includes("/exchange")){
+  if (url === "/") {
+    handlers.handlerHome(req, res);
+  } else if (url.includes("/exchange")) {
     handlers.handlerExchange(req, res);
-  }
-
-  else if (["/style.css", "dom.js"].includes(url)){
+  } else if (["/style.css", "dom.js"].includes(url)) {
     handlers.handlerPublic(req, res, url);
+  } else {
+    handlers.handler404(req, res);
   }
+};
 
-  else {
-    handlers.handler404(req, res)
-  }
-}
-
-module.export = router
+module.exports = router;
