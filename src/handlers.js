b493317e38cd3636bb950580dada0e107d1e79b1
Ben Newman <benjamin-newman@live.co.uk>
2018-07-31 17:49:36 +0300

    #9

--- src/handlers.js
+++ src/handlers.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const logic = require('./logic');
+// const logic = require('./logic');
 
 const handler500 = function (res) {
   res.writeHead(500, { 'content-type': 'text/plain' });
@@ -24,12 +24,14 @@ const handlerPublic = function (req, res, url) {
     html: 'text/html',
     css: 'text/css',
     js: 'application/javascript',
-    jpg: 'image/jpeg',
+    jpg: 'image/jpg',
+    gif: 'image/gif',
   };
   fs.readFile(path.join(__dirname, '..', 'public', url), (err, file) => {
     if (err) {
       handler500(res);
     } else {
+      console.log(extensionType[extension]);
       res.writeHead(200, { 'Content-Type': extensionType[extension] });
       res.end(file);
     }
