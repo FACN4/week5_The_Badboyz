95a48fb7bca409b1b7b20eded7889e6911266f88
Ben Newman <benjamin-newman@live.co.uk>
2018-08-01 14:14:30 +0300

    request added

--- src/httpReq.js
+++ src/httpReq.js
@@ -1,7 +1,10 @@
-// const request = require('request');
-//
-// const httpReq = (query) => {
-//   const APIurl = `https://free.currencyconverterapi.com/api/v6/convert?q=${query}`;
-// };
-//
-// module.exports = httpReq;
+const request = require('request');
+
+const httpReq = (query) => {
+  const APIurl = `https://free.currencyconverterapi.com/api/v6/convert?q=${query}`;
+  request(APIurl, (error, response, body) => {
+    console.log(body);
+  });
+};
+
+module.exports = httpReq;
